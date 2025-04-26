f4ef4357bf4d1ef6509f2d7d3e85b2bf18d55c80
Arvind1212 <arvindhsk1212@gmail.com>
2025-04-26 19:04:52 +0530

    change mail

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "s3_lambda_sns" {
   destination_bucket_name = "parquet-destination-bucket-arvind-123"
   lambda_function_name    = "csv-to-parquet-lambda"
   sns_topic_name          = "csv-to-parquet-lambda-topic"
-  sns_email_address       = "your-email@example.com"
+  sns_email_address       = "arvindhsk1212@gmail.com"
 
   lambda_role_name        = "lambda_csv_to_parquet_exec_role_v6"
   lambda_policy_name      = "lambda_csv_to_parquet_policy_v6"
